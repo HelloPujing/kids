a51ade635706e61f946b961ae1a8df8c20a03870
fuqing <fuqing@qunhemail.com>
2022-10-08 09:48:31 +0800

    Initial Commit

---
+++ miniprogram/app.ts
@@ -0,0 +1,22 @@
+// app.ts
+// Pupuu https://developers.weixin.qq.com/miniprogram/dev/framework/app-service/app.html
+// App({}) reigister global data and funcs
+// getApp() get instance
+
+App<IAppOption>({
+  globalData: {},
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res.code)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      },
+    })
+  },
+})
