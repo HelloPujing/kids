9478ce92fdb6664a2d103f9f39cb6afeef04df73
fuqing <fuqing@qunhemail.com>
2022-10-11 11:11:41 +0800

    style(sprint10): nickname & tags

--- miniprogram/pages/create/create.ts
+++ miniprogram/pages/create/create.ts
@@ -2,10 +2,45 @@
 // 获取应用实例
 // Pupuu https://developers.weixin.qq.com/miniprogram/dev/framework/app-service/page.html
 // Page({}) register data、customer data、events
+const TAGS = [
+  {
+    name: 'friend’s kid',
+    color: '#F1857B'
+  },
+  {
+    name: 'relative’s kid',
+    color: '#FAD85A'
+  },
+  {
+    name: 'colleague’s kid',
+    color: '#A5DE80'
+  },
+  {
+    name: 'schoolmate’s kid',
+    color: '#BAE6F1'
+  },
+  {
+    name: 'neighbour’s kid',
+    color: '#C8ACD5'
+  },
+  {
+    name: 'his/her friend',
+    color: '#F79F77'
+  },
+];
+
 Page({
   data: {
-    fullName: '',
     nickName: '',
+    tags: TAGS,
+    category: '',
+    categories: [
+      {
+        name: '亲戚家',
+        color: '#'
+      }
+    ],
+    fullName: '',
     gender: 1,
     birth: ''
   },
