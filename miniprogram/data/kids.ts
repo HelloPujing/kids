59b27503e81b645a7d076e318d94f6b1e701ec3a
Pupuu <hellopujing@qq.com>
2022-11-11 13:44:44 +0800

    feature(sprint10): style - add button

--- miniprogram/data/kids.ts
+++ miniprogram/data/kids.ts
@@ -1,76 +1,87 @@
 let kids: Kid[] = [
   {
     id: '0',
-    fullname: '易星旻',
+    fullname: '示例：小咔咔',
     nickname: 'Bianca',
     gender: 0, // 0 femail 1 mail
     birthday: '2017/04/08',
-    tagId: '-1',
-  },
-  {
-    id: '1',
-    fullname: '小来来',
-    nickname: '小来来',
-    gender: 1, // 0 femail 1 mail
-    birthday: '2017/01/09',
     tagId: '1',
   },
-  {
-    id: '2',
-    fullname: '毛镱霏',
-    nickname: '囡囡',
-    gender: 0, // 0 femail 1 mail
-    birthday: '2014/03/16',
-    tagId: '1',
-  },
-  {
-    id: '3',
-    fullname: '许愿',
-    nickname: '小愿愿',
-    gender: 0, // 0 femail 1 mail
-    birthday: '2014/08/12',
-    tagId: '1',
-  },
-  {
-    id: '4',
-    fullname: '许末',
-    nickname: '末末头',
-    gender: 1, // 0 femail 1 mail
-    birthday: '2016/09/01',
-    tagId: '1',
-  },
-  {
-    id: '5',
-    fullname: '徐陟尹',
-    nickname: '葱葱',
-    gender: 1, // 0 femail 1 mail
-    birthday: '2012/12/23',
-    tagId: '2',
-  },
-  {
-    id: '6',
-    fullname: '徐芷伊',
-    nickname: '双双',
-    gender: 0, // 0 femail 1 mail
-    birthday: '2014/1/12',
-    tagId: '2',
-  },
-  {
-    id: '7',
-    fullname: '钱悠然',
-    nickname: '悠悠',
-    gender: 0, // 0 femail 1 mail
-    birthday: '2011/09/14',
-    tagId: '1',
-  },
-  {
-    id: '8',
-    fullname: '钱羿然',
-    nickname: '羿宝',
-    gender: 1, // 0 femail 1 mail
-    birthday: '2014/07/25',
-    tagId: '1',
-  },
-] 
+]
+
+// let kids: Kid[] = [
+//   {
+//     id: '0',
+//     fullname: '易星旻',
+//     nickname: 'Bianca',
+//     gender: 0, // 0 femail 1 mail
+//     birthday: '2017/04/08',
+//     tagId: '-1',
+//   },
+//   {
+//     id: '1',
+//     fullname: '小来来',
+//     nickname: '小来来',
+//     gender: 1, // 0 femail 1 mail
+//     birthday: '2017/01/09',
+//     tagId: '1',
+//   },
+//   {
+//     id: '2',
+//     fullname: '毛镱霏',
+//     nickname: '囡囡',
+//     gender: 0, // 0 femail 1 mail
+//     birthday: '2014/03/16',
+//     tagId: '1',
+//   },
+//   {
+//     id: '3',
+//     fullname: '许愿',
+//     nickname: '小愿愿',
+//     gender: 0, // 0 femail 1 mail
+//     birthday: '2014/08/12',
+//     tagId: '1',
+//   },
+//   {
+//     id: '4',
+//     fullname: '许末',
+//     nickname: '末末头',
+//     gender: 1, // 0 femail 1 mail
+//     birthday: '2016/09/01',
+//     tagId: '1',
+//   },
+//   {
+//     id: '5',
+//     fullname: '徐陟尹',
+//     nickname: '葱葱',
+//     gender: 1, // 0 femail 1 mail
+//     birthday: '2012/12/23',
+//     tagId: '2',
+//   },
+//   {
+//     id: '6',
+//     fullname: '徐芷伊',
+//     nickname: '双双',
+//     gender: 0, // 0 femail 1 mail
+//     birthday: '2014/1/12',
+//     tagId: '2',
+//   },
+//   {
+//     id: '7',
+//     fullname: '钱悠然',
+//     nickname: '悠悠',
+//     gender: 0, // 0 femail 1 mail
+//     birthday: '2011/09/14',
+//     tagId: '1',
+//   },
+//   {
+//     id: '8',
+//     fullname: '钱羿然',
+//     nickname: '羿宝',
+//     gender: 1, // 0 femail 1 mail
+//     birthday: '2014/07/25',
+//     tagId: '1',
+//   },
+// ] 
 
 module.exports = kids;
