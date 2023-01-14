684a0f89e8bc279390732f65f2aa82b39edb25f8
Pupuu <hellopujing@qq.com>
2023-01-14 14:54:17 +0800

    config(sprint10): test/prod

---
+++ miniprogram/config/hosts.t.ts
@@ -0,0 +1,9 @@
+export const HOST_API = 'https://kid-book-api.t.metahmo.com/api';
+
+// 系统图片
+export const HOST_CDN_IMAGE_SYS = 'http://kid-cdn.metahmo.com';
+
+// UGC图片
+export const HOST_CDN_IMAGE_UGC = 'https://kid-cdn-ugc-t.metahmo.com'
+
+export const HOST_BUCKET_IMAGE_UGC = 'https://kid-book-ugc-t.oss-cn-hangzhou.aliyuncs.com'
